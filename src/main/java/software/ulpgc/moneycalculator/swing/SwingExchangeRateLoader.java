67f7298f3927d9845f6838f5eaf4fcb2bd7e7ab0
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2024-01-19 13:08:46 +0000

    Partimos de la versión del proyecto

---
+++ src/main/java/software/ulpgc/moneycalculator/swing/SwingExchangeRateLoader.java
@@ -0,0 +1,12 @@
+package software.ulpgc.moneycalculator.swing;
+
+import software.ulpgc.moneycalculator.ExchangeRateLoader;
+import software.ulpgc.moneycalculator.model.Currency;
+import software.ulpgc.moneycalculator.model.ExchangeRate;
+
+public class SwingExchangeRateLoader implements ExchangeRateLoader {
+    @Override
+    public ExchangeRate load(Currency from, Currency to) {
+        return null;
+    }
+}
